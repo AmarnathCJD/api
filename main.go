76439956970324aaf1d4fea258ab23208ee607c5
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2022-04-12 21:13:02 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "http"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	http.HandleFunc("/ping", func (w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("pong"))
+	}
 }
