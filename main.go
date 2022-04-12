47ab73db0f01d4d9048836a1f0b622e7e0c9035a
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2022-04-12 21:18:47 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,4 +6,5 @@ func main() {
 	http.HandleFunc("/ping", func (w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("pong"))
 	}
+	http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil)
 }
